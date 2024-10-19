7d67f96b43a1e7d88bfc1f65359c130ea5f0df0c
AlexisDev <alexisescobar1234@gmail.com>
2024-10-19 16:09:45 -0300

    configurando el proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'header': 'url(./src/assets/img/foto2.jpg)',
+        'cepram': 'url(./src/assets/img/foto1.jpg)',
+      },
+      backgroundColor: {
+        'header-color': 'rgba(25, 77, 65, 0.712);',
+        'cepram-color': 'rgba(94, 27, 156, 0.808)',
+        'upami-color': 'rgba(0, 47, 111, 0.27)',
+        'elIllia-color': 'rgba(25, 126, 0, 0.54)'
+      },
+      colors: {
+        'texto': 'rgb(255, 255, 255)',
+        'darkBlue': '#0B2344',
+        'darkGreen': '#197E00'
+      }
+    },
+  },
+  plugins: [],
+}
+
