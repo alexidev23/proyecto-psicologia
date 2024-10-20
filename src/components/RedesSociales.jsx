32ec26de0e1beb1ae7a7f38c457a527b4058b482
AlexisDev <alexisescobar1234@gmail.com>
2024-10-20 10:56:36 -0300

    correcion en los estilos del componente redes sociales en la vista desktop

--- src/components/RedesSociales.jsx
+++ src/components/RedesSociales.jsx
@@ -3,7 +3,7 @@ export function RedesSociales ({ redes }) {
   return (
     <div className="flex flex-col lg:flex-row gap-10 lg:gap-0 lg:justify-between">
       {redes.map(({ id, img, name, url, color }) => (
-        <a href={url} key={id} target="_blank" rel="noopener noreferrer" className={`flex flex-col lg:flex-row gap-2 items-center justify-center py-4 border border-[${color}] rounded-3xl`}>
+        <a href={url} key={id} target="_blank" rel="noopener noreferrer" className={`flex flex-col lg:flex-row gap-2 items-center justify-center py-4 border border-[${color}] rounded-3xl lg:px-10`}>
           <img src={`/${img}`} alt={url} height={35} width={35} />
           <span className="font-medium text-lg lg:text-xl">{name}</span>
         </a>
